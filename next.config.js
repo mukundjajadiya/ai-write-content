c2172f071a36d7d1277705152ead533dd89e0915
mukund jajadiya <mukundjajadiya2017@gmail.com>
2022-03-24 11:40:10 +0530

    V 1.0.0

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
 
-module.exports = nextConfig
+const nextConfig = {
+	reactStrictMode: true,
+	env: {
+		BASE_URL: process.env.BASE_URL,
+	},
+};
+
+module.exports = nextConfig;
